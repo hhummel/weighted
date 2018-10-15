ed0562ebe31bb2841634413a2081ade0e3544cad
Harry Hummel <hhhummel@comcast.net>
2018-10-15 00:17:44 -0400

    Fixed root urls

--- w8d/urls.py
+++ w8d/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from a import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:responder>', views.default_redirection, name='default_redirection'),
+    path('', views.default_redirection, name='default_redirection'),
+    path('<str:responder>', views.default_redirection, name='redirection'),
 ]
 
