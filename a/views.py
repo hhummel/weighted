3cb8dbf827a8c6adafd7f1bef00174278fb2b03e
Harry Hummel <hhhummel@comcast.net>
2018-10-16 17:38:30 -0400

    Cast target to str in views

--- a/views.py
+++ a/views.py
@@ -15,7 +15,7 @@ def redirection(request, target, responder):
     if target == 0:
         return index(request)
     try:
-        obj = Targets.objects.get(index=int(target))
+        obj = Targets.objects.get(index=str(target))
 
         #Save the reponse event the Response table
         response = Response()
