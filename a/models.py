28f3eca9d417a635ad741027ef27559181152160
Harry Hummel <hhhummel@comcast.net>
2018-10-16 15:27:35 -0400

    Change column index to index_key in a_targets

--- a/models.py
+++ a/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Targets(models.Model):
-    index = models.CharField(max_length=15, primary_key=True)
+    index_key = models.CharField(max_length=15, primary_key=True)
     url = models.URLField(max_length=200, null=False)
 
 class Response(models.Model):
